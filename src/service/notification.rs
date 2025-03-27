f3633b9b85b66c4a46b3ff9cefb928bb429aaf90
naylafn <naylafarah.31@gmail.com>
2025-03-28 00:05:09 +0700

    Implement unsubscribe function in Notification service

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -1,19 +1,29 @@
 use std::thread;
 
-use bambangshop::{Result, compose_error_response};
-use rocket::http::Status;
 use crate::model::notification::Notification;
 use crate::model::product::Product;
 use crate::model::subscriber::Subscriber;
 use crate::repository::subscriber::SubscriberRepository;
+use bambangshop::{compose_error_response, Result};
+use rocket::http::Status;
 
 pub struct NotificationService;
 
-impl NotificationService{
-    pub fn subscribe(product_type: &str, subscriber: Subscriber) -> Result<Subscriber>{
+impl NotificationService {
+    pub fn subscribe(product_type: &str, subscriber: Subscriber) -> Result<Subscriber> {
         let product_type_upper: String = product_type.to_uppercase();
         let product_type_str: &str = &product_type_upper.as_str();
-        let subscriber_result : Subscriber = SubscriberRepository::add(product_type_str, subscriber);
+        let subscriber_result: Subscriber = SubscriberRepository::add(product_type_str, subscriber);
         return Ok(subscriber_result);
+    }
+
+    pub fn unsubscribe(product_type: &str, url: &str) -> Result<Subscriber>{
+        let product_type_upper: String = product_type.to_uppercase();
+        let product_type_str: &str = &product_type_upper.as_str();
+        let result: Option<Subscriber> = SubscriberRepository::delete(product_type_str, url);
+        if result.is_none(){
+            return Err (compose_error_response(Status::NotFound,String::from("Subscriber Not Found")));
+        }
+        return Ok(result.unwrap());
     }
 }
